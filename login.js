f3514fce31d3f745ed399319bb6bdd147e14d877
Nur <nuruddinnur538@gmail.com>
2023-08-15 17:50:29 +0600

    added some text

--- login.js
+++ login.js
@@ -1,9 +1,15 @@
 document.getElementById('btn-submit').addEventListener('click', function () {
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
-
+    
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
     console.log(email, password);
-
+    if (email === 'sontan@gmail.com' && password === 'secret') {
+        window.location.href = 'bank.html'
+    }
+    else {
+        alert('Invalid')
+    }
+    emailField.value = '';
 })
